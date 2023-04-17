108d96e9aa938aa8510db7719b3c20816c2b1fcc
Mattias Ross <fuuijin@gmail.com>
2023-04-17 23:08:31 +0200

    meeeer

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -17,7 +17,7 @@ launch_bar() {
 	elif [[ "$style" == "pwidgets" ]]; then
 		bash "$dir"/pwidgets/launch.sh --main
 	else
-		polybar -r -q main -c "$dir/$style/config.ini" &	
+		polybar -q main -c "$dir/$style/config.ini" &	
 	fi
 }
 
@@ -61,18 +61,6 @@ elif [[ "$1" == "--forest" ]]; then
 	style="forest"
 	launch_bar
 
-elif [[ "$1" == "--pwidgets" ]]; then
-	style="pwidgets"
-	launch_bar
-
-elif [[ "$1" == "--panels" ]]; then
-	style="panels"
-	launch_bar
-
-elif [[ "$1" == "--untitled" ]]; then
-	style="untitled"
-	launch_bar
-
 else
 	cat <<- EOF
 	Usage : launch.sh --theme
@@ -80,6 +68,6 @@ else
 	Available Themes :
 	--blocks    --colorblocks    --cuts      --docky
 	--forest    --grayblocks     --hack      --material
-	--panels    --pwidgets       --shades    --shapes
+	--shades    --shapes
 	EOF
 fi
