4db4b3513c8c19fd66888fe81ee02e074d39a412
Mattias Ross <fuuijin@gmail.com>
2023-05-05 09:40:27 +0200

    Fix polybar

--- bin/bspwm_setup_monitors.sh
+++ bin/bspwm_setup_monitors.sh
@@ -49,8 +49,8 @@ fi
 killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 2; done
 if [[ $(xrandr -q | grep "${EXTERNAL_MONITOR} connected") ]]; then
-  polybar --reload primary -c ~/.config/polybar/forest/config.ini </dev/null >/var/tmp/polybar-primary.log 2>&1 200>&- &
+  polybar --reload main -c ~/.config/polybar/forest/config.ini </dev/null >/var/tmp/polybar-primary.log 2>&1 200>&- &
   polybar --reload secondary -c ~/.config/polybar/forest/config.ini </dev/null >/var/tmp/polybar-secondary.log 2>&1 200>&- &
 else
-  polybar --reload primary -c ~/.config/polybar/forest/config.ini </dev/null >/var/tmp/polybar-primary.log 2>&1 200>&- &
+  polybar --reload main -c ~/.config/polybar/forest/config.ini </dev/null >/var/tmp/polybar-primary.log 2>&1 200>&- &
 fi
